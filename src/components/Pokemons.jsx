0d2a91b89378ac1e09e4920b3a713a4c8a2670cb
everitdmarvin <marvin.orsua@everho.ph>
2022-11-21 14:53:20 +0800

    final

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,29 +1,21 @@
-import { Layout, Row, Col, Typography, Spin, Progress, Pagination, Button } from 'antd'
+import { Layout, Row, Col, Typography, Spin, Progress, Pagination } from 'antd'
 import React, { useContext, useEffect, useState } from 'react'
 import { PokeContext } from '../context/pokemonContext'
-import Modal from './Modal'
 import { LoadingOutlined } from '@ant-design/icons';
 import { colorFilter } from '../functions/color';
 import { Link } from 'react-router-dom';
 const { Title, Text } = Typography
 
-const Pokemon = ({ data, handleModal }) => {
-   const {stats, sprites, types} = data;
-   
+const Pokemon = ({ data }) => {
    let style = {
       padding: '10px',
       borderRadius:'3px'
    }
-   const { fetchPokemon } = useContext(PokeContext)
-   const pokemonInfo = () => {
-      handleModal()
-      fetchPokemon(data.name)
-   }
-
+   const {stats, sprites, types} = data
    return (
       <Col className="gutter-row" xs={24} md={12} xl={6} style={{ padding:'5px'}} >
-         <div onClick={pokemonInfo} style={{cursor: 'pointer', boxShadow: '8px 11px 5px -4px rgba(0,0,0,0.19)'}}>
-            <Row style={colorFilter( style, types[0].type.name)}>
+         <Link to={`/${data.name}`} >
+            <Row style={ colorFilter( style, types[0].type.name)}>
                <Col span={15} style={{paddingRight:'10px'}}>
                   <Title level={3} style={{textTransform:'capitalize', fontWeight:'bolder'}}>{data.name}</Title>
                   {
@@ -41,7 +33,7 @@ const Pokemon = ({ data, handleModal }) => {
                   <img src={sprites.front_default} style={{width:'7em'}}/>
                </Col>
             </Row>
-         </div>
+         </Link>
       </Col>   
    )
 }
@@ -57,7 +49,6 @@ const antIcon = (
 
 const Pokemons = () => {
    const { pokemons } = useContext(PokeContext)
-   const [open, setOpen] = useState(false)
    const [currentList, setCurrentList] = useState();
    const [currentPage, setCurrentPage] = useState(1);
    const [perPage, setPerPage] = useState(20);
@@ -78,29 +69,23 @@ const Pokemons = () => {
    }
    const setPageLimit = (current, size) => {
       setPerPage(size)
+      // console.log(current, size)
    }
 
-   const handleModal = () => {
-      setOpen(!open)
-      console.log(open)
-   }
    return (
-      <React.Fragment>
-         <Layout style={{padding: ' 1rem 4rem', height: 'calc(100vh - 64px)', overflowX: 'scroll', backgroundColor:'white'}}>
-            <Row gutter={14}>
-               {
-                  currentList ? currentList.map((pokemon, index) => {
-                     return <Pokemon data={pokemon}  key={index} handleModal={handleModal}/>
-                  }) : <Spin indicator={antIcon} />
-               }
-            </Row>
-            <Row style={{display:'flex', justifyContent:'center', marginTop:'1rem'}}>
-               <Pagination defaultCurrent={currentPage} pageSize={perPage} total={1000} onChange={setPagination} onShowSizeChange={setPageLimit} />
-            </Row>
-            
-         </Layout>
-         <Modal open={open} handleModal={handleModal}/>
-      </React.Fragment>
+      <Layout style={{padding: ' 1rem 4rem', height: 'calc(100vh - 64px)', overflowX: 'scroll', backgroundColor:'white'}}>
+         <Row gutter={14}>
+            {
+               currentList ? currentList.map((pokemon, index) => {
+                  return <Pokemon data={pokemon} key={index}/>
+               }) : <Spin indicator={antIcon} />
+            }
+         </Row>
+         <Row>
+            <Pagination defaultCurrent={currentPage} pageSize={perPage} total={1000} onChange={setPagination} onShowSizeChange={setPageLimit}/>
+         </Row>
+
+      </Layout>
    )
 }
 
